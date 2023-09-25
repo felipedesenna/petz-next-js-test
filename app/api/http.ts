34e7053cdc9060a4b29d363be91a62fe16bb1157
Felipe de Senna <lipe.senna@hotmail.com>
2023-09-25 22:22:31 +0100

    fix: corretion call post time

--- app/api/http.ts
+++ app/api/http.ts
@@ -8,9 +8,13 @@ export async function getAppointmentDate(): Promise<Appointment> {
   return data;
 }
 
-export async function getAppointmentTime(date: string): Promise<Appointment> {
+export async function getAppointmentHour(value: string): Promise<Appointment> {
   const response = await fetch('http://localhost:3000/api/time', {
-    body: JSON.stringify(date)
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ date: value }),
   });
   const data = await response.json();
   return data;
