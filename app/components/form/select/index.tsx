8a4c2887d23f33bb8c54571bc858ae615387d973
Felipe de Senna <lipe.senna@hotmail.com>
2023-09-25 22:31:13 +0100

    fix: select component to including change event

--- app/components/form/select/index.tsx
+++ app/components/form/select/index.tsx
@@ -1,10 +1,11 @@
-import { SelectHTMLAttributes } from 'react'
+import { ChangeEvent, SelectHTMLAttributes } from 'react'
 
 type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
   id: string
   text: string
   defaultText: string
   options: OptionsProps[]
+  onSelected?: (value: ChangeEvent<HTMLSelectElement>) => void
 }
 
 export type OptionsProps = {
@@ -12,11 +13,23 @@ export type OptionsProps = {
   value: string
 }
 
-export function Select({ id, text, defaultText, options, ...rest }: SelectProps) {
+export function Select({
+  id,
+  text,
+  defaultText,
+  onSelected,
+  options,
+  ...rest
+}: SelectProps) {
   return (
     <div className="container_input_select">
       <label htmlFor={id}>{text}</label>
-      <select name={id} id={id} defaultValue={0} {...rest}>
+      <select
+        id={id}
+        defaultValue="0"
+        onChange={onSelected}
+        {...rest}
+      >
         <option value="0" disabled>{defaultText}</option>
         {options.map(item => (
           <option key={item.id} value={item.id}>{item.value}</option>
