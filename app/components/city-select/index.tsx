0524e117a263a207cf56cb5bcddc43950d64c1d1
Felipe de Senna <lipe.senna@hotmail.com>
2023-09-26 13:06:04 +0100

    fix: sorting out region-and-city in two components

---
+++ app/components/city-select/index.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+
+import { Select } from '@/components/form/select'
+import { getPokemonCity } from '@/api/http'
+import { RegionResult } from '@/interfaces/api'
+import { mapperSelect } from '@/utils/mapperSelect'
+
+type CitySelectProps = {
+  onCityValue: (value: ChangeEvent<HTMLSelectElement>) => void
+  regionId: string
+}
+
+export function CitySelect({ onCityValue, regionId }: CitySelectProps) {
+  const [cityData, setCityData] = useState<RegionResult[]>([])
+
+  async function loadAppointmentCity(region: string) {
+    const valueLowerCase = region.toLocaleLowerCase()
+    const { locations } = await getPokemonCity(valueLowerCase)
+    setCityData(locations)
+  }
+
+  useEffect(() => {
+    loadAppointmentCity(regionId)
+  }, [regionId])
+
+  const cityToAppointment = mapperSelect(cityData)
+
+  return (
+    <Select
+      id="city"
+      text="Cidade"
+      defaultText="Selecione sua cidade"
+      options={cityToAppointment}
+      onSelected={onCityValue}
+    />
+  )
+}
---
+++ app/components/region-select/index.tsx
@@ -0,0 +1,35 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+
+import { Select } from '@/components/form/select'
+import { getPokemonRegion } from '@/api/http'
+import { RegionResult } from '@/interfaces/api'
+import { mapperSelect } from '@/utils/mapperSelect'
+
+type RegionSelectProps = {
+  onRegionValue: (value: ChangeEvent<HTMLSelectElement>) => void
+}
+
+export function RegionSelect({ onRegionValue }: RegionSelectProps) {
+  const [regionData, setRegionData] = useState<RegionResult[]>([])
+
+  async function loadAppointmentRegion() {
+    const { results } = await getPokemonRegion()
+    setRegionData(results)
+  }
+
+  useEffect(() => {
+    loadAppointmentRegion()
+  }, [])
+
+  const regionToAppointment = mapperSelect(regionData)
+
+  return (
+    <Select
+      id="region"
+      text="Região"
+      defaultText="Selecione sua região"
+      options={regionToAppointment}
+      onSelected={onRegionValue}
+    />
+  )
+}
