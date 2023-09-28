d973e8611c645da819eed8ead51e34343d40edf2
Felipe de Senna <lipe.senna@hotmail.com>
2023-09-28 10:30:54 +0100

    feat: completion form to create new appointment and transform hour value

--- app/components/form/index.tsx
+++ app/components/form/index.tsx
@@ -1,53 +1,175 @@
+'use client'
+
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react'
+
+import { ErrorInfo } from '@/components/error-info'
+import { SuccessInfo } from '@/components/success-info'
 import { Input } from '@/components/form/input'
 import { Button } from '@/components/form/button'
 import { CreatePokemon } from '@/components/create-pokemon'
-import { AppointmentSelect } from '@/components/appointment-select'
-import { RegionAndCitySelect } from '@/components/region-and-city-select'
+import { AppointmentDateSelect } from '@/components/appointment-date-select'
+import { AppointmentHourSelect } from '@/components/appointment-hour-select'
+import { RegionSelect } from '@/components/region-select'
+import { CitySelect } from '@/components/city-select'
 import { Summary } from '@/components/summary'
+import { convertCurrency, formatHourAndMinutes } from '@/utils/transform'
 
 import styles from '@/components/form/styles.module.css'
 
-async function handleSubmit(formData: FormData) {
-  'use server'
+type TeamPokemon = {
+  id: string
+  value: string
+}
 
-  const name = formData.get("name")
-  console.log(name)
+type StatusMessage = {
+  status: 'success' | 'error' | 'wanting'
+  message?: string
 }
 
 export function Form() {
+  const unitaryValue = 70
+  const fee = 2.1
+
+  const [appointmentDate, setAppointmentDate] = useState<ChangeEvent<HTMLSelectElement>>()
+  const [appointmentHour, setAppointmentHour] = useState<ChangeEvent<HTMLSelectElement>>()
+  const [region, setRegion] = useState<ChangeEvent<HTMLSelectElement>>()
+  const [city, setCity] = useState<ChangeEvent<HTMLSelectElement>>()
+  const [newPokemon, setNewPokemon] = useState<ChangeEvent<HTMLSelectElement>>()
+  const [teamPokemon, setTeamPokemon] = useState<TeamPokemon[]>([])
+  const [statusMessage, setStatusMessage] = useState<StatusMessage>({ status: 'wanting' })
+
+  const nameInputRef = useRef<HTMLInputElement>(null)
+  const lastNameInputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (!newPokemon?.target.id || !newPokemon?.target.value) {
+      return
+    }
+
+    const id = newPokemon.target.id
+    const value = newPokemon.target.value
+
+    setTeamPokemon(state => [
+      ...state,
+      { id, value }
+    ])
+  }, [newPokemon?.target])
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const payload = {
+      name: nameInputRef.current?.value,
+      lastName: lastNameInputRef.current?.value,
+      region: region?.target.value,
+      city: city?.target.value,
+      team: teamPokemon.length,
+      date: appointmentDate?.target.value,
+      hour: formatHourAndMinutes(appointmentHour?.target.value),
+    }
+
+    if (teamPokemon.length === 0) {
+      setStatusMessage({
+        status: 'error',
+        message: 'Todos os campos devem ser preenchidos e ter ao menos um pokémon adicionado ao time.',
+      })
+    } else {
+      setStatusMessage({
+        status: 'success',
+        message: `Seu agendamento para dia ${payload.date}, às ${payload.hour}, para ${payload.team}x pokémons foi realizado com sucesso!`
+      })
+    }
+
+    /* more info if necessary
+    console.log(payload)
+    console.log(`Treinador pokémon: ${nameInputRef.current?.value} ${lastNameInputRef.current?.value}`)
+    console.log(`Pokémon da região ${region?.target.value} e cidade ${city?.target.value}`)
+    teamPokemon.forEach((item, index) => (
+      console.log(`Pokémons que vão se consultar: ${index + 1} - ${item.value}`)
+    ))
+    */
+  }
+
+  function newAppointment() {
+    setStatusMessage({
+      status: 'wanting',
+    })
+  }
+
   return (
-    <form className={styles.container} action={handleSubmit}>
-      <div className={styles.content}>
-        <Input
-          id="name"
-          text="Nome"
-          placeholder="Digite seu nome"
+    <>
+      {statusMessage.status === 'success' && (
+        <SuccessInfo
+          message={statusMessage.message ?? ''}
+          onNewAppointment={newAppointment}
         />
-        <Input
-          id="last_name"
-          text="Sobrenome"
-          placeholder="Digite seu sobrenome"
+      )}
+
+      {statusMessage.status === 'error' && (
+        <ErrorInfo
+          message={statusMessage.message ?? ''}
+          onNewAppointment={newAppointment}
         />
-      </div>
+      )}
 
-      <div className={styles.content}>
-        <RegionAndCitySelect />
-      </div>
+      {statusMessage.status === 'wanting' && (
+        <form className={styles.container} onSubmit={handleSubmit}>
+          <div className={styles.content}>
+            <Input
+              id="name"
+              text="Nome"
+              placeholder="Digite seu nome"
+              onChangeValue={nameInputRef}
+            />
+            <Input
+              id="last_name"
+              text="Sobrenome"
+              placeholder="Digite seu sobrenome"
+              onChangeValue={lastNameInputRef}
+            />
+          </div>
 
-      <CreatePokemon />
+          <div className={styles.content}>
+            <RegionSelect onRegionValue={setRegion} />
 
-      <div className={styles.content}>
-        <AppointmentSelect />
-      </div>
+            {region?.target.value && (
+              <CitySelect
+                onCityValue={setCity}
+                regionId={region.target.value}
+              />
+            )}
+          </div>
 
-      <span className={styles.separator} />
+          <CreatePokemon onPokemonValue={setNewPokemon} />
 
-      <Summary />
+          <div className={styles.content}>
+            <AppointmentDateSelect onDateValue={setAppointmentDate} />
 
-      <div className={styles.total}>
-        <p className={styles.value}>Valor Total: R$ 72,10</p>
-        <Button type="submit">Concluir Agendamento</Button>
-      </div>
-    </form>
+            {appointmentDate?.target.value && (
+              <AppointmentHourSelect
+                onHourValue={setAppointmentHour}
+                dateId={appointmentDate.target.value}
+              />
+            )}
+          </div>
+
+          <span className={styles.separator} />
+
+          <Summary
+            quantity={teamPokemon.length}
+            unitaryValue={unitaryValue}
+            fee={fee}
+          />
+
+          <div className={styles.total}>
+            <p className={styles.value}>
+              Valor Total:{' '}
+              {convertCurrency(teamPokemon.length * unitaryValue + fee)}
+            </p>
+            <Button type="submit">Concluir Agendamento</Button>
+          </div>
+        </form>
+      )}
+    </>
   )
 }
--- app/utils/transform.ts
+++ app/utils/transform.ts
@@ -8,11 +8,21 @@ export function capitalizeLetter(str: string): string {
   return removeDash.charAt(0).toUpperCase() + removeDash.slice(1);
 }
 
-export function convertCurrency(currency: number) {
+export function convertCurrency(currency: number): string {
   if (!currency) {
     currency = 0
   }
 
   return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
     .format(currency);
+}
+
+export function formatHourAndMinutes(hour: string | undefined): string {
+  if (!hour) {
+    return ''
+  }
+
+  const [hours, minutes] = hour.split(':');
+
+  return `${hours}h${minutes}m`;
 }
