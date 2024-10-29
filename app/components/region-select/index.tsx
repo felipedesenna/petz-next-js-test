90d79fce5f77b0e662964fcd1fe15ee700c67b18
Felipe de Senna <felipe.de.senna@outlook.com>
2024-10-29 22:50:21 +0000

    first commit

--- app/components/region-select/index.tsx
+++ app/components/region-select/index.tsx
@@ -1,27 +1,16 @@
-import { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent } from 'react'
 
 import { Select } from '@/components/form/select'
 import { getPokemonRegion } from '@/api/http'
-import { Result } from '@/interfaces/api'
 import { mapperSelect } from '@/utils/mapperSelect'
 
 type RegionSelectProps = {
   onRegionValue: (value: ChangeEvent<HTMLSelectElement>) => void
 }
 
-export function RegionSelect({ onRegionValue }: RegionSelectProps) {
-  const [regionData, setRegionData] = useState<Result[]>([])
-
-  async function loadAppointmentRegion() {
-    const { results } = await getPokemonRegion()
-    setRegionData(results)
-  }
-
-  useEffect(() => {
-    loadAppointmentRegion()
-  }, [])
-
-  const regionToAppointment = mapperSelect(regionData)
+export async function RegionSelect({ onRegionValue }: RegionSelectProps) {
+  const { results } = await getPokemonRegion()
+  const regionToAppointment = mapperSelect(results)
 
   return (
     <Select
